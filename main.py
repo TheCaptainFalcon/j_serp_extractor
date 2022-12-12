1cb37a6c9ed87e35e3bf14c48fcc5363c7332cf7
Joseph <josephkim570@gmail.com>
2022-12-11 20:19:13 -0500

    Initial creation of db and sample insertion of data functioning correctly.

--- main.py
+++ main.py
@@ -21,11 +21,33 @@ database = os.getenv('DATABASE')
 api_key = os.getenv('SERPAPI_KEY')
 
 
-# json_source = 'https://serpapi.com/searches/c2747cffe0cd36d6/6396359cf26ac6a892cab1ca.json'
-# j = requests.get(url=json_source)
-# content = json.loads(j.content)
+# python 3+ need to install pymysql and add to mysql
+engine = create_engine('mysql+pymysql://{0}:{1}@{2}:{3}/{4}'.format(user, password, host, port, database))
 
-# df = pd.DataFrame.from_dict(content['job_results'])
-# df = pd.concat([pd.DataFrame(df), pd.json_normalize(df['detected_extensions'])], axis=1).drop(columns='detected_extensions')
+# sample data from source
+json_source = 'https://serpapi.com/searches/c2747cffe0cd36d6/6396359cf26ac6a892cab1ca.json'
+j = requests.get(url=json_source)
+content = json.loads(j.content)
+
+# normalizing the detected_extensions into a readable/usable column
+df = pd.DataFrame.from_dict(content['jobs_results'])
+df = pd.concat([pd.DataFrame(df), pd.json_normalize(df['detected_extensions'])], axis=1).drop(columns='detected_extensions')
+# need to remove as it repeats detected extensions but is a list -- cannot use normalize (and is pointless)
+df = df.drop(columns='extensions', axis=1)
+
+# print(df.head(5))
+
+jobs_df = df
+
+# create table in mysql db -- only needed for initial setup
+# engine.execute('create database data_jobs')
+
+# change to append after finalising sample source
+jobs_df.to_sql('jobs', con=engine, if_exists='append')
+
+# checking if returns data correctly
+# print(engine.execute('SELECT title FROM jobs').fetchall())
+
+
 
 
