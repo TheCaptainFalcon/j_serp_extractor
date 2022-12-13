fba2a65bccb67bd1f5fac159aba8c23320c4f12b
Joseph <josephkim570@gmail.com>
2022-12-12 21:57:37 -0500

    Tested individual select queries and checked to make sure conditional filtering was applying correctly. Combined all the individual queries into a multi-step CTE to reduce repetition and further filter the results. Keeping the comments in this commit just as a reference; will delete moving forward.

--- main.py
+++ main.py
@@ -55,23 +55,122 @@ yoe =  [
 # print(engine.execute('SELECT title FROM jobs').fetchall())
 
 # unbelievable... song and dance needed just to pass the % wildcard. Need to do this multiple times tho...
-tech_stack_filter = (
-    "SELECT title, company_name FROM jobs WHERE description LIKE %(sql)s OR description LIKE %(excel)s OR description LIKE %(tableau)s OR description LIKE %(python)s"
-    )
+# tech_stack_filter = (
+#     "SELECT title, company_name FROM jobs WHERE description LIKE %(sql)s OR description LIKE %(excel)s OR description LIKE %(tableau)s OR description LIKE %(python)s"
+# )
 
-# sql = {'%' + 'sql' + '%'}
-# excel = {'%' + 'excel' + '%'}
+# yoe_filter = (
+#     "SELECT title, company_name FROM jobs WHERE description LIKE %(yoe_0)s OR description LIKE %(yoe_1)s OR description LIKE %(yoe_2)s  OR description LIKE %(yoe_3)s"
+# )
 
-tech_jobs = pd.read_sql(
-    tech_stack_filter, 
-    con=engine, 
-    params={
-        'sql': '%' + 'sql' + '%',
-        'excel' : '%' + 'excel' + '%',
-        'tableau' : '%' + 'tableau' + '%',
-        'python' : '%' + 'python' + '%'
-    })
+# non_masters_filter = (
+#     "SELECT title, company_name FROM jobs WHERE description NOT LIKE %(masters_1)s AND description NOT LIKE %(masters_2)s AND description NOT LIKE %(masters_3)s AND description NOT LIKE %(masters_4)s"
+# )
 
-print(tech_jobs)
+combination_filter = (
+    "WITH tech_stack as ( \
+        SELECT \
+            title, \
+            company_name, \
+            description \
+        from jobs \
+        WHERE description LIKE %(sql)s OR \
+        description LIKE %(excel)s OR \
+        description LIKE %(tableau)s OR \
+        description LIKE %(python)s \
+    ), \
+    yoe as ( \
+        SELECT \
+            title, \
+            company_name, \
+            description \
+        FROM tech_stack \
+        WHERE description LIKE %(yoe_0)s OR \
+        description LIKE %(yoe_1)s OR \
+        description LIKE %(yoe_2)s OR \
+        description LIKE %(yoe_3)s \
+    ) \
+    SELECT \
+        title, \
+        company_name \
+    FROM yoe \
+    WHERE description NOT LIKE %(masters_1)s AND \
+    description NOT LIKE %(masters_2)s AND \
+    description NOT LIKE %(masters_3)s AND \
+    description NOT LIKE %(masters_4)s"
+)
+
+# tech_stack_dict = {
+#     'sql' : '%' + 'sql' + '%',
+#     'excel' : '%' + 'excel' + '%',
+#     'tableau' : '%' + 'tableau' + '%',
+#     'python' : '%' + 'python' + '%'
+# }
+
+# yoe_dict = {
+#     'yoe_0' : '%' + '0' + '%',
+#     'yoe_1' : '%' + '1' + '%',
+#     'yoe_2' : '%' + '2' + '%',
+#     'yoe_3' : '%' + '3' + '%'
+# }
+
+# __ accounts for if they use apostrophe after the s (plus a space)
+
+# non_masters_dict = {
+#     'masters_1' : '%' + "master's" + '%', 
+#     'masters_2' : '%' + "master's__degree" + '%', 
+#     'masters_3' : '%' + "masters_degree" + '%', 
+#     'masters_4' : '%' + 'masters_in' + '%'
+# }
+
+combination_dict = {
+    'sql' : '%' + 'sql' + '%',
+    'excel' : '%' + 'excel' + '%',
+    'tableau' : '%' + 'tableau' + '%',
+    'python' : '%' + 'python' + '%',
+    'yoe_0' : '%' + '0' + '%',
+    'yoe_1' : '%' + '1' + '%',
+    'yoe_2' : '%' + '2' + '%',
+    'yoe_3' : '%' + '3' + '%',
+    'masters_1' : '%' + "master's" + '%', 
+    'masters_2' : '%' + "master's__degree" + '%', 
+    'masters_3' : '%' + "masters_degree" + '%', 
+    'masters_4' : '%' + 'masters_in' + '%'
+}
+
+
+# tech_jobs = pd.read_sql(
+#     tech_stack_filter, 
+#     con=engine, 
+#     params=tech_stack_dict
+# )
+
+# print(tech_jobs)
+
+# yoe_jobs = pd.read_sql(
+#     yoe_filter,
+#     con=engine,
+#     params=yoe_dict
+# )
+
+# print(yoe_jobs)
+
+# non_masters_jobs = pd.read_sql(
+#     non_masters_filter,
+#     con=engine,
+#     params=non_masters_dict
+# )
+
+# print(non_masters_jobs)
+
+combination_jobs = pd.read_sql(
+    combination_filter,
+    con=engine,
+    params=combination_dict
+)
+
+print(combination_jobs)
+
+
 
 
