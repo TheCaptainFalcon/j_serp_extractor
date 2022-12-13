ec892eafebe0c10257f234351a049a3df4fcf252
Joseph <josephkim570@gmail.com>
2022-12-12 23:05:44 -0500

    Implemented the export to csv function with proper error/exception report handling; the function occurs after the filtering has been segmented into a variable. Working as expected.

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import requests
 from sqlalchemy import create_engine
 from dotenv import load_dotenv
 import os
+import traceback
 
 # pathing to direct the env vars
 current_path = os.getcwd()
@@ -20,7 +21,6 @@ port = os.getenv('PORT')
 database = os.getenv('DATABASE')
 api_key = os.getenv('SERPAPI_KEY')
 
-
 # python 3+ need to install pymysql and add to mysql
 engine = create_engine('mysql+pymysql://{0}:{1}@{2}:{3}/{4}'.format(user, password, host, port, database))
 
@@ -35,38 +35,13 @@ df = pd.concat([pd.DataFrame(df), pd.json_normalize(df['detected_extensions'])],
 # need to remove as it repeats detected extensions but is a list -- cannot use normalize (and is pointless)
 df = df.drop(columns='extensions', axis=1)
 
-tech_stack = [
-    'sql', 'excel', 'tableau', 'python'
-]
-
-yoe =  [
-    '0', '1', '2', '3'
-]
-
-# print(df.head(5))
-
 # create table in mysql db -- only needed for initial setup
 # engine.execute('create database data_jobs')
 
 # change to append after finalising sample source
-# jobs_df = df.to_sql('jobs', con=engine, if_exists='append')
+# df.to_sql('jobs', con=engine, if_exists='append')
 
-# checking if returns data correctly
-# print(engine.execute('SELECT title FROM jobs').fetchall())
-
-# unbelievable... song and dance needed just to pass the % wildcard. Need to do this multiple times tho...
-# tech_stack_filter = (
-#     "SELECT title, company_name FROM jobs WHERE description LIKE %(sql)s OR description LIKE %(excel)s OR description LIKE %(tableau)s OR description LIKE %(python)s"
-# )
-
-# yoe_filter = (
-#     "SELECT title, company_name FROM jobs WHERE description LIKE %(yoe_0)s OR description LIKE %(yoe_1)s OR description LIKE %(yoe_2)s  OR description LIKE %(yoe_3)s"
-# )
-
-# non_masters_filter = (
-#     "SELECT title, company_name FROM jobs WHERE description NOT LIKE %(masters_1)s AND description NOT LIKE %(masters_2)s AND description NOT LIKE %(masters_3)s AND description NOT LIKE %(masters_4)s"
-# )
-
+# In order to use wildcards, must use %(insert name)s -- dictionary can be setup within the params flag, but its cleaner to separate into a variable
 combination_filter = (
     "WITH tech_stack as ( \
         SELECT \
@@ -100,29 +75,7 @@ combination_filter = (
     description NOT LIKE %(masters_4)s"
 )
 
-# tech_stack_dict = {
-#     'sql' : '%' + 'sql' + '%',
-#     'excel' : '%' + 'excel' + '%',
-#     'tableau' : '%' + 'tableau' + '%',
-#     'python' : '%' + 'python' + '%'
-# }
-
-# yoe_dict = {
-#     'yoe_0' : '%' + '0' + '%',
-#     'yoe_1' : '%' + '1' + '%',
-#     'yoe_2' : '%' + '2' + '%',
-#     'yoe_3' : '%' + '3' + '%'
-# }
-
 # __ accounts for if they use apostrophe after the s (plus a space)
-
-# non_masters_dict = {
-#     'masters_1' : '%' + "master's" + '%', 
-#     'masters_2' : '%' + "master's__degree" + '%', 
-#     'masters_3' : '%' + "masters_degree" + '%', 
-#     'masters_4' : '%' + 'masters_in' + '%'
-# }
-
 combination_dict = {
     'sql' : '%' + 'sql' + '%',
     'excel' : '%' + 'excel' + '%',
@@ -138,39 +91,21 @@ combination_dict = {
     'masters_4' : '%' + 'masters_in' + '%'
 }
 
-
-# tech_jobs = pd.read_sql(
-#     tech_stack_filter, 
-#     con=engine, 
-#     params=tech_stack_dict
-# )
-
-# print(tech_jobs)
-
-# yoe_jobs = pd.read_sql(
-#     yoe_filter,
-#     con=engine,
-#     params=yoe_dict
-# )
-
-# print(yoe_jobs)
-
-# non_masters_jobs = pd.read_sql(
-#     non_masters_filter,
-#     con=engine,
-#     params=non_masters_dict
-# )
-
-# print(non_masters_jobs)
-
 combination_jobs = pd.read_sql(
     combination_filter,
     con=engine,
     params=combination_dict
 )
 
-print(combination_jobs)
+# print(combination_jobs)
 
+def filtered_jobs_to_csv():
+    try:
+        combination_jobs.to_csv('filtered_jobs.csv')
+    except Exception:
+        print('An error occurred trying to export the filtered job results to a csv', '\n')
+        traceback.print_exc()
+    else:
+        print('Filtered job CSV has been updated!')
 
-
-
+filtered_jobs_to_csv()
