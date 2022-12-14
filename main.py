26a2c6ac1e459f51110216a798c00e593cd50653
Joseph <josephkim570@gmail.com>
2022-12-14 13:50:23 -0500

    Added more specifications to the read query as part of the csv export. Unnamed column would appear unless the index is set to False. Currently testing the csv on pandas with jupyter and datapane (automated report) before importing to production code.

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from sqlalchemy import create_engine
 from dotenv import load_dotenv
 import os
 import traceback
+from datetime import date
 
 # pathing to direct the env vars
 current_path = os.getcwd()
@@ -35,11 +36,14 @@ df = pd.concat([pd.DataFrame(df), pd.json_normalize(df['detected_extensions'])],
 # need to remove as it repeats detected extensions but is a list -- cannot use normalize (and is pointless)
 df = df.drop(columns='extensions', axis=1)
 
+# to distinguish data point for when script inserted this record into db
+df['inserted_at'] = date.today()
+
 # create table in mysql db -- only needed for initial setup
 # engine.execute('create database data_jobs')
 
 # change to append after finalising sample source
-# df.to_sql('jobs', con=engine, if_exists='append')
+# df.to_sql('jobs', con=engine, if_exists='replace')
 
 # In order to use wildcards, must use %(insert name)s -- dictionary can be setup within the params flag, but its cleaner to separate into a variable
 combination_filter = (
@@ -47,7 +51,12 @@ combination_filter = (
         SELECT \
             title, \
             company_name, \
-            description \
+            location, \
+            via, \
+            description, \
+            posted_at, \
+            substring_index(posted_at, ' ', 1) as posted_at_int, \
+            inserted_at \
         from jobs \
         WHERE description LIKE %(sql)s OR \
         description LIKE %(excel)s OR \
@@ -58,7 +67,14 @@ combination_filter = (
         SELECT \
             title, \
             company_name, \
-            description \
+            location, \
+            via, \
+            description, \
+            CASE \
+                WHEN posted_at LIKE %(hour)s THEN ROUND(posted_at_int / 24, 2) \
+                ELSE posted_at_int \
+            END AS posted_by_day, \
+            inserted_at \
         FROM tech_stack \
         WHERE description LIKE %(yoe_0)s OR \
         description LIKE %(yoe_1)s OR \
@@ -66,8 +82,12 @@ combination_filter = (
         description LIKE %(yoe_3)s \
     ) \
     SELECT \
-        title, \
-        company_name \
+            title, \
+            company_name, \
+            location, \
+            via, \
+            posted_by_day, \
+            inserted_at \
     FROM yoe \
     WHERE description NOT LIKE %(masters_1)s AND \
     description NOT LIKE %(masters_2)s AND \
@@ -88,7 +108,8 @@ combination_dict = {
     'masters_1' : '%' + "master's" + '%', 
     'masters_2' : '%' + "master's__degree" + '%', 
     'masters_3' : '%' + "masters_degree" + '%', 
-    'masters_4' : '%' + 'masters_in' + '%'
+    'masters_4' : '%' + 'masters_in' + '%',
+    'hour' : '%' + 'hour' + '%'
 }
 
 combination_jobs = pd.read_sql(
@@ -101,7 +122,7 @@ combination_jobs = pd.read_sql(
 
 def filtered_jobs_to_csv():
     try:
-        combination_jobs.to_csv('filtered_jobs.csv')
+        combination_jobs.to_csv('filtered_jobs.csv', index=False)
     except Exception:
         print('An error occurred trying to export the filtered job results to a csv', '\n')
         traceback.print_exc()
